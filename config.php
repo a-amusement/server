99d86ac3cf4834a033d0b58f55304b8d90c3fbf0
Thomas <internetralsei@gmail.com>
2026-06-21 11:04:12 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ return [
     'db' => [
         'host' => 'localhost',
         'port' => 5432,
-        'name' => 'my_webapp__13',
-        'user' => 'my_webapp__13',
-        'password' => '5LiPvmNzFCoIO4MKr4EZ3fNyO6BWMc',
+        'name' => 'YOUR DB NAME HERE',
+        'user' => 'YOUR DB USERNAME HERE',
+        'password' => 'YOUR DB PASSWD HERE',
     ],
     'app' => [
         'name' => 'a-amusement',
